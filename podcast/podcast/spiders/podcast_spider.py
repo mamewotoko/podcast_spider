a627c1fffc05ab866283773c5ebb38b2ddebd6e2
Takashi Masuyama <mamewotoko@gmail.com>
2015-11-23 07:49:05 +0900

    initial commit

---
+++ podcast/podcast/spiders/podcast_spider.py
@@ -0,0 +1,42 @@
+import urlparse
+import scrapy
+from podcast.items import PodcastItem
+
+class PodcastSpider(scrapy.Spider):
+    name = "podcast"
+    allowed_domains = ["tfm.co.jp", "fmtoyama.co.jp"]
+#    start_urls = [ "http://www.tfm.co.jp/podcasts/",
+#                   "http://www.fmtoyama.co.jp/" ]
+    start_urls = [ "http://www.fmtoyama.co.jp/" ]
+    visited = []
+
+    def parse(self, response):
+        self.logger.info("parse: "+response.url)
+        self.visited.append(response.url)
+        content_type = response.headers['Content-Type']
+        if content_type.startswith("text/html"):
+            links = response.xpath("//a/@href").extract()
+            podcast_pages = filter(lambda x: "podcast" in x, list(set(links)))
+            for url in podcast_pages:
+                next_url = response.urljoin(url.strip())
+                if next_url not in self.visited and not next_url.endswith(".mp3"):
+                    self.logger.info("next_podcast_pages: " + next_url)
+                    yield scrapy.Request(next_url, callback=self.parse)
+        elif content_type.startswith("application/rss+xml") or content_type.startswith("text/xml"):
+            title = response.xpath("//channel/title/text()").extract()[0]
+            link = response.xpath("//channel/link/text()").extract()[0]
+            enctag = response.xpath("//enclosure")
+            has_audio = False
+            for tag in enctag:
+                mimetypel = tag.xpath("@type").extract()
+                if len(mimetypel) > 0 and mimetypel[0].startswith("audio/"):
+                    has_audio = True
+                    break
+            self.logger.info("hasAudio: "+ str(has_audio) + " " + response.url)
+            if has_audio:
+                item = PodcastItem()
+                item['title'] = title
+                item['url'] = response.url.decode('utf-8')
+                item['link'] = link
+                yield item
+               
